2b63adc3006a25bf9154f98679f6a36a64dfcdbd
Venkat Malladi <vmalladi@microsoft.com>
2024-04-01 11:48:25 -0500

    Fix security group rules.

--- azure_license-server/security_group/security_group_rules.tf
+++ azure_license-server/security_group/security_group_rules.tf
@@ -12,11 +12,12 @@ resource "azurerm_network_security_group" "security_group" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    source_address_prefix      = "172.31.0.0/16"
-    destination_address_prefix = "*"
-    destination_port_range     = "8990"
+    source_address_prefix      = "*"
     source_port_range          = "*"
+    destination_address_prefix = azurerm_linux_virtual_machine.license_server_instance.private_ip_address
+    destination_port_range     = "8990"
   }
+
 
   security_rule {
     name                       = "AllowLicenseServerOutboundHTTPS"
@@ -24,7 +25,7 @@ resource "azurerm_network_security_group" "security_group" {
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "Tcp"
-    destination_address_prefix = "52.89.132.242"
+    destination_address_prefix = "52.89.132.242/32""
     destination_port_range     = "443"
     source_address_prefix      = "*"
     source_port_range          = "*"
@@ -36,15 +37,10 @@ resource "azurerm_network_security_group" "security_group" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
+    destination_address_prefix = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
+    source_port_range          = "*"
   }
 
-  security_rule {
-    name                       = "AllowLicenseServerICMP"
-    priority                   = 130
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Icmp"
-  }
 }
